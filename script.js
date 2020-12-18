a69fa09e395a4e49cb26c70f8379a3d65b5d39d7
Alexandr Stepanenko <alexstep07@icloud.com>
2020-12-18 02:03:40 +0200

    initial commit

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
-function smallestCommons(arr) {
-  let from = Math.min(...arr);
-  let to = Math.max(...arr);
-  let diff = to - from;
-  let range = [];
-  let mult = 1;
-  while (range.length < (diff + 1)) {
-    range.push(from);
-    from++;
-  }
-  while (!range.every( e => ( mult % e === 0 ) ) ) {
-    mult++;
-  }
-  return mult;
+const main = document.querySelector(".main");
+const url = "https://randomuser.me/api/?results=50";
+
+const fetchFriends = async (url) => {
+	let response = await fetch(url);
+	let data = await response.json();
+	console.log(data.results);
+	return data.results;
+};
+
+fetchFriends(url).then((friends) =>
+	friends.forEach((friend) => render(friend))
+);
+
+function render(friend) {
+	const card = document.createElement("div");
+	const img = document.createElement("img");
+	const name = document.createElement("p");
+	img.setAttribute("src", friend.picture.large);
+	img.classList.add("card__img");
+	name.textContent = `${friend.name.first} ${friend.name.last}`;
+    name.classList.add("card__name");
+    card.classList.add("card");
+	card.append(img, name);
+	main.append(card);
 }
-
-
-console.log(smallestCommons([1,13]));
