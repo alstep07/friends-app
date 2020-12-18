a6e97b708a93badac24e47478a1083b7058b2069
Alexandr Stepanenko <alexstep07@icloud.com>
2020-12-18 15:06:27 +0200

    add init function

--- script.js
+++ script.js
@@ -1,26 +1,39 @@
 const main = document.querySelector(".main");
-const url = "https://randomuser.me/api/?results=50";
+const url = "https://randomuser.me/api/?results=70";
 
 const fetchFriends = async (url) => {
-	let response = await fetch(url);
-	let data = await response.json();
-	console.log(data.results);
-	return data.results;
+	try {
+		let response = await fetch(url);
+		let data = await response.json();
+		return data.results;
+	} catch (err) {
+		main.innerHTML = `<div class="error">server not responding<br>(error: ${err})</div>`;
+	}
 };
 
-fetchFriends(url).then((friends) =>
-	friends.forEach((friend) => render(friend))
-);
+const init = async () => {
+	const results = await fetchFriends(url);
+	results.forEach((elem) => render(elem));
+};
+
+init();
 
 function render(friend) {
 	const card = document.createElement("div");
 	const img = document.createElement("img");
 	const name = document.createElement("p");
+	const age = document.createElement("p");
+
+	name.innerHTML = `${friend.name.first}<br> ${friend.name.last}`;
+	age.innerHTML = `age: ${friend.dob.age} years`;
 	img.setAttribute("src", friend.picture.large);
+
+	card.classList.add("card");
+	card.classList.add(`${friend.gender}`);
 	img.classList.add("card__img");
-	name.textContent = `${friend.name.first} ${friend.name.last}`;
-    name.classList.add("card__name");
-    card.classList.add("card");
-	card.append(img, name);
+	name.classList.add("card__name");
+	age.classList.add("card__age");
+
+	card.append(img, name, age);
 	main.append(card);
 }
