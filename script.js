b99858b39f881bbe0a244191f189c152dd46a650
Alexandr Stepanenko <alexstep07@icloud.com>
2021-02-18 01:00:55 +0200

    change form html

--- script.js
+++ script.js
@@ -4,12 +4,13 @@ const url = `https://randomuser.me/api/?results=${MAX_FRIENDS_VALUE}&noinfo`;
 const phoneImg = "./img/phone.png";
 const mailImg = "./img/mail.png";
 const errMessage = "server is not responding";
+const form = document.querySelector('#form');
 const searchInput = document.querySelector("#search");
-const filterGender = document.querySelector(".search__by-gender");
-const filterName = document.querySelector(".search__by-alphabet");
-const filterAge = document.querySelector(".search__by-age");
-const filterDate = document.querySelector(".search__by-date");
-const resetBtn = document.querySelector(".search__reset__btn");
+// const filterGender = document.querySelector(".search__by-gender");
+// const filterName = document.querySelector(".search__by-alphabet");
+// const filterAge = document.querySelector(".search__by-age");
+// const filterDate = document.querySelector(".search__by-date");
+// const resetBtn = document.querySelector(".search__reset__btn");
 const popupBtn = document.querySelector(".popup");
 const navbar = document.querySelector(".navbar");
 const filters = {
@@ -44,39 +45,9 @@ const init = (results) => {
 		filterCards(data);
 	});
 
-	filterGender.addEventListener("click", ({ target }) => {
-		filters.gender = target.id;
-		filterCards(data);
-	});
-
-	filterName.addEventListener("click", ({ target }) => {
-		filters.name = target.id;
-		filters.age = null;
-		filters.date = null;
-		filterCards(data);
-	});
-
-	filterAge.addEventListener("click", ({ target }) => {
-		filters.age = target.id;
-		filters.name = null;
-		filters.date = null;
-		filterCards(data);
-	});
-
-	filterDate.addEventListener("click", ({ target }) => {
-		filters.date = target.id;
-		filters.age = null;
-		filters.name = null;
-		filterCards(data);
-	});
-
-	resetBtn.addEventListener("click", () => {
-		filters.search = null;
-		filters.gender = null;
-		filters.name = null;
-		filters.age = null;
-		render(results);
-	});
+	form.addEventListener('change', ({target}) => {
+		console.log(target.value);
+	})
 };
 
 function filterCards(data) {
