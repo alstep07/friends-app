8c1b37d7db936204c51a209ab3ad373f61eb2745
Alexandr Stepanenko <alexstep07@icloud.com>
2021-02-18 16:18:12 +0200

    add all filters

--- script.js
+++ script.js
@@ -1,36 +1,33 @@
 const MAX_FRIENDS_VALUE = 100;
-const main = document.querySelector(".main");
-const url = `https://randomuser.me/api/?results=${MAX_FRIENDS_VALUE}&noinfo`;
-const phoneImg = "./img/phone.png";
-const mailImg = "./img/mail.png";
-const errMessage = "server is not responding";
+const URL = `https://randomuser.me/api/?results=${MAX_FRIENDS_VALUE}&noinfo`;
+
+const main = document.querySelector('.main');
+const navbar = document.querySelector('.navbar');
 const form = document.querySelector('#form');
-const searchInput = document.querySelector("#search");
-// const filterGender = document.querySelector(".search__by-gender");
-// const filterName = document.querySelector(".search__by-alphabet");
-// const filterAge = document.querySelector(".search__by-age");
-// const filterDate = document.querySelector(".search__by-date");
-// const resetBtn = document.querySelector(".search__reset__btn");
-const popupBtn = document.querySelector(".popup");
-const navbar = document.querySelector(".navbar");
-const filters = {
+
+const phoneImg = './img/phone.png';
+const mailImg = './img/mail.png';
+const errMessage = 'server is not responding';
+
+const resetButton = document.querySelector('#reset');
+const popupButton = document.querySelector('.popup');
+
+const filterState = {
 	search: null,
 	gender: null,
-	age: null,
-	name: null,
-	date: null,
+	sort: null,
+	reset() {
+		this.search = null;
+		this.gender = null;
+		this.sort = null;
+	},
 };
-
-popupBtn.addEventListener("click", () => {
-	popupBtn.classList.toggle("popup-active");
-	navbar.classList.toggle("navbar-active");
-})
 
 const fetchFriends = async (url) => {
 	try {
 		let response = await fetch(url);
-		let data = await response.json();
-		return data.results;
+		let usersData = await response.json();
+		return usersData.results;
 	} catch (err) {
 		main.innerHTML = `<div class="error">${errMessage}<br>(error: ${err})</div>`;
 	}
@@ -40,86 +37,119 @@ const init = (results) => {
 	render(results);
 	const users = results.slice();
 
-	form.addEventListener('input', (event) => {
-		filterCards(users, target.name, target.value);
-	})
+	form.addEventListener('input', ({ target }) => {
+		filterState[target.name] = target.value;
+		filterCards(users, filterState);
+	});
+
+	resetButton.addEventListener('click', () => {
+		filterState.reset();
+		render(results);
+	});
 };
 
-function filterCards(usersToFilter, filterType, filterValue) {
+const filterCards = (usersToFilter, filters) => {
 	let users = usersToFilter;
-	switch (filterType) {
-		case "gender":
-			users = filterByGender(users, filterValue);
-			break;
+
+	if (filterState.search) {
+		users = filterBySearch(users, filters.search);
+	}
+	if (filterState.gender) {
+		users = filterByGender(users, filters.gender);
+	}
+	if (filterState.sort) {
+		let [type, direction] = filters.sort.split(' ');
+		users = sortUsers(users, type, direction);
 	}
 	render(users);
-}
+};
 
-function sortBySearch(dataToFilter, searchValue) {
+const filterBySearch = (dataToFilter, searchValue) => {
 	return dataToFilter.filter((elem) =>
 		`${elem.name.first}${elem.name.last}${elem.location.country}${elem.location.city}`
 			.toLowerCase()
-			.includes(searchValue.toLowerCase())
+			.includes(searchValue.toLowerCase()),
 	);
-}
+};
 
-function filterByGender(dataToFilter, gender) {
-	if (gender === "all") {
-		return dataToFilter;
+const filterByGender = (usersToFilter, gender) => {
+	if (gender === 'all') {
+		return usersToFilter;
 	}
-	return dataToFilter.filter((elem) => elem.gender === gender);
-}
+	return usersToFilter.filter((user) => user.gender === gender);
+};
 
-function sortByName(dataToFilter, direction) {
-	if (direction === "name_a-z") {
-		return dataToFilter.sort((a, b) => a.name.first.localeCompare(b.name.first));
-	} else if (direction === "name_z-a")
-		return dataToFilter.sort((a, b) => b.name.first.localeCompare(a.name.first));
-}
-
-function sortByAge(dataToFilter, direction) {
-	if (direction === "age_up") {
-		return dataToFilter.sort((a, b) => a.dob.age - b.dob.age);
-	} else if (direction === "age_down") {
-		return dataToFilter.sort((a, b) => b.dob.age - a.dob.age);
+const sortUsers = (usersToSort, sortType, sortDirection) => {
+	let users = usersToSort;
+	switch (sortType) {
+		case 'age':
+			users = sortByAge(usersToSort, sortDirection);
+			break;
+		case 'name':
+			users = sortByName(usersToSort, sortDirection);
+			break;
+		case 'date':
+			users = sortByDate(usersToSort, sortDirection);
+			break;
 	}
-}
+	return users;
+};
 
-function sortByDate(dataToFilter, direction) {
-	if (direction === "date_up") {
-		return dataToFilter.sort((a, b) => a.registered.age - b.registered.age);
-	} else if (direction === "date_down") {
-		return dataToFilter.sort((a, b) => b.registered.age - a.registered.age);
+const sortByName = (usersToFilter, direction) => {
+	if (direction === 'asc') {
+		return usersToFilter.sort((a, b) => a.name.first.localeCompare(b.name.first));
+	} else if (direction === 'desc')
+		return usersToFilter.sort((a, b) => b.name.first.localeCompare(a.name.first));
+};
+
+const sortByAge = (usersToFilter, direction) => {
+	if (direction === 'asc') {
+		return usersToFilter.sort((a, b) => a.dob.age - b.dob.age);
+	} else if (direction === 'desc') {
+		return usersToFilter.sort((a, b) => b.dob.age - a.dob.age);
 	}
-}
+};
 
-function render(dataToRender) {
-	main.innerHTML = "";
-	const container = document.createDocumentFragment();
-	dataToRender.forEach((elem) => container.append(createCard(elem)));
-	main.append(container);
-}
+const sortByDate = (usersToFilter, direction) => {
+	if (direction === 'asc') {
+		return usersToFilter.sort((a, b) => a.registered.age - b.registered.age);
+	} else if (direction === 'desc') {
+		return usersToFilter.sort((a, b) => b.registered.age - a.registered.age);
+	}
+};
 
-function createCard(friend) {
-	const card = document.createElement("div");
+const render = (usersToRender) => {
+	main.innerHTML = '';
+	const usersContainer = document.createDocumentFragment();
+	usersToRender.forEach((user) => usersContainer.append(createCard(user)));
+	main.append(usersContainer);
+};
+
+const createCard = (user) => {
+	const card = document.createElement('div');
 	card.innerHTML = `
-		<div class="card">
-			<img src="${friend.picture.large}" alt="photo" class="card__img">
-			<p class="card__name">${friend.name.first} ${friend.name.last}</p>
-			<p class="card__age">${friend.dob.age} years</p>
-			<p class="card__location">${friend.location.country}<br>${friend.location.city}</p>
-			<div class="card__contacts">
-				<a class="card__link" href="tel: ${friend.cell}">
+		<article class="card">
+			<img src="${user.picture.large}" alt="photo" class="card__img">
+			<h2 class="card__name">${user.name.first} ${user.name.last}</h2>
+			<p class="card__age">${user.dob.age} years</p>
+			<p class="card__location">${user.location.country}<br>${user.location.city}</p>
+			<address class="card__contacts">
+				<a class="card__link" href="tel: ${user.cell}">
 					<img class="card__phone__img" src="${phoneImg}" alt = "phone to">
 				</a>
-				<a class="card__link" href="mailto: ${friend.email}">
+				<a class="card__link" href="mailto: ${user.email}">
 					<img class="card__mail__img" src="${mailImg}" alt = "mail to">
 				</a>
-			</div>
-		</div>`;
+			</address>
+		</article>`;
 	return card;
-}
+};
 
-document.addEventListener("DOMContentLoaded", () => {
-	fetchFriends(url).then(init);
+popupButton.addEventListener('click', () => {
+	popupButton.classList.toggle('popup-active');
+	navbar.classList.toggle('navbar-active');
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+	fetchFriends(URL).then(init);
 });
